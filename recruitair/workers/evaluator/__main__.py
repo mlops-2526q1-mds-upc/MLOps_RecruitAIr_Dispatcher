30bd92871defc0ae2cc0f905373bd1b57d02a22d
AimbotParce <marcparcerisa@gmail.com>
2025-11-29 17:31:33 +0100

    First compose version of the workers and logging

--- recruitair/workers/evaluator/__main__.py
+++ recruitair/workers/evaluator/__main__.py
@@ -2,5 +2,8 @@ from . import evaluator_worker
 
 if __name__ == "__main__":
     import asyncio
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
 
     asyncio.run(evaluator_worker())
--- recruitair/workers/extractor/__main__.py
+++ recruitair/workers/extractor/__main__.py
@@ -2,5 +2,8 @@ from . import extractor_worker
 
 if __name__ == "__main__":
     import asyncio
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
 
     asyncio.run(extractor_worker())
